971d9a5074aeb3a1d53c35b7dae9379313d07d66
bereket <bekibirhanu8693@gmail.com>
2024-09-06 16:41:41 +0300

    the final commit on task one and 2

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,7 @@
 from scripts.useroverview import UserOverview
 from scripts.db_setup import DataLoading
 from scripts.data_cleanming_and_preprocessing import data_cleaning_and_preprocessing
+from scripts.userOverviewOverCleanedData import UserOverviewovercleaneddata
+from scripts.overviewOverSpecificApplication import overviewoversomeapplication
+from scripts.edaForAgeregatedData import eda_for_agg_data
+from scripts.customer_engagment import CustomerEngagement
